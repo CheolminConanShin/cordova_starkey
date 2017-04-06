db546149ffeb44e41445d211fee437a51da58a3f
actmember <actmember@stjnpq9312w-lp140-03-70-50-170-218.dsl.bell.ca>
2017-04-07 08:39:34 +0900

    Customer Table with div tags

--- client/components/CustomerTable.js
+++ client/components/CustomerTable.js
@@ -3,6 +3,15 @@ var PropTypes = React.PropTypes;
 import {Grid, Row, Col, Clearfix} from 'react-bootstrap'
 const dummySentences = ['Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'Donec hendrerit tempor tellus.', 'Donec pretium posuere tellus.', 'Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Nulla posuere.', 'Donec vitae dolor.', 'Nullam tristique diam non turpis.', 'Cras placerat accumsan nulla.', 'Nullam rutrum.', 'Nam vestibulum accumsan nisl.'];
 
+var styles = {
+  color1: {
+    backgroundColor: "lavender"
+  },
+  color2: {
+    backgroundColor: "lavenderblush"
+  }
+}
+
 const gridInstance = (
   <Grid>
     <Row className="show-grid">
@@ -16,10 +25,28 @@ const gridInstance = (
 );
 
 const tableInstance = (
-  <div className="row">
-    <div className="col-xs-4" style={{"background-color:lavender"}}>.col-sm-4</div>
-    <div className="col-xs-4" style={{background-color:"lavenderblush"}}>.col-sm-4</div>
-    <div className="col-xs-4" style={{background-color:"lavender"}}>.col-sm-4</div>
+  <div>
+    <div className="row">
+      <div className="col-xs-2" style={styles.color1}>name</div>
+      <div className="col-xs-2" style={styles.color2}>date</div>
+      <div className="col-xs-4" style={styles.color1}>address</div>
+      <div className="col-xs-2" style={styles.color2}>telephone</div>
+      <div className="col-xs-2" style={styles.color1}>mobilephone</div>
+    </div>
+    <div className="row">
+      <div className="col-xs-2" style={styles.color1}>Conan Shin</div>
+      <div className="col-xs-2" style={styles.color2}>2015-11-30</div>
+      <div className="col-xs-4" style={styles.color1}>용산구 한강대로 98다길 26</div>
+      <div className="col-xs-2" style={styles.color2}>010-1111-1111</div>
+      <div className="col-xs-2" style={styles.color1}>010-1111-1111</div>
+    </div>
+    <div className="row">
+      <div className="col-xs-2" style={styles.color1}>Conan Shin</div>
+      <div className="col-xs-2" style={styles.color2}>2015-11-30</div>
+      <div className="col-xs-4" style={styles.color1}>용산구 한강대로 98다길 26</div>
+      <div className="col-xs-2" style={styles.color2}>010-1111-1111</div>
+      <div className="col-xs-2" style={styles.color1}>010-1111-1111</div>
+    </div>
   </div>
 )
 
